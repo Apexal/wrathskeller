6d3a5795e053e8daf72f263d1d362691aabed933
Frank Matranga <thefrankmatranga@gmail.com>
2021-12-30 01:24:39 -0500

    Create theme with fonts

--- Menu.gd
+++ Menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_StartButton_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://levels/Level1.tscn")
 
 func _on_LevelSelectButton_pressed():
 	pass # Replace with function body.
