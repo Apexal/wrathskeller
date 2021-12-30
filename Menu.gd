9b45ad4a30eecd716983c934ac53177b45d85ef7
Frank Matranga <thefrankmatranga@gmail.com>
2021-12-30 00:53:03 -0500

    Add signal handlers

---
+++ Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_StartButton_pressed():
+	pass # Replace with function body.
+
+func _on_LevelSelectButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	# Quit the game
+	get_tree().quit()
+
+
