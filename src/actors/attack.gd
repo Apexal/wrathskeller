e867fbc634ce732aea769239d08bcd08d5594728
Frank Matranga <thefrankmatranga@gmail.com>
2022-01-10 21:00:11 -0500

    Add attack damage variable

--- src/actors/attack.gd
+++ src/actors/attack.gd
@@ -1,7 +1,7 @@
 class_name Attack
 extends Node
 
-export(String) var attack_name := ""
 export(String) var animation_name := ""
+export(float) var damage := 10.0
 export(AudioStream) var default_audio
 export(Dictionary) var per_character_audio
