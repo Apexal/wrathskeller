ce3be6da1f0977b95e82e073c68489a77d5d0a91
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-05 23:59:46 -0500

    Display preview animations

--- src/scenes/characters/manage_characters/ManageCharacter.gd
+++ src/scenes/characters/manage_characters/ManageCharacter.gd
@@ -1,25 +1,16 @@
+tool
 extends Control
 
 export(PackedScene) var character_template: PackedScene
 
-
-
-
-func _deserialize_characters():
-	var characters = []
-	var dir = Directory.new()
-	if dir.open("user://") == OK:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir():
-				if file_name.ends_with(".scn"):
-					var character = load("user://" + file_name).instance()
-					characters.append(character)
-			file_name = dir.get_next()
-	
-	return characters
-
 func _ready():
-	var animated_sprite = CharacterManager.create_character_preview(CharacterManager.load_character_json("out.json"))
-	$GridContainer.add_child(animated_sprite)
+	for character_file in CharacterManager.list_character_files():		
+		var container := VBoxContainer.new()
+		var character_data = CharacterManager.load_character_json(character_file)
+		var animated_sprite = CharacterManager.create_character_preview(character_data)
+		container.add_child(animated_sprite)
+		var label := Label.new()
+		label.text = character_data["name"]
+		container.add_child(label)
+		
+		$GridContainer.add_child(container)
--- src/scripts/CharacterManager.gd
+++ src/scripts/CharacterManager.gd
@@ -2,6 +2,19 @@ extends Node
 
 const character_template = preload("res://src/actors/players/Character.tscn")
 
+static func list_character_files() -> Array:
+	var character_files = []
+	var dir = Directory.new()
+	if dir.open("user://") == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir():
+				if file_name.ends_with(".json"):
+					character_files.append(file_name)
+			file_name = dir.get_next()
+	return character_files
+
 static func load_character(file_path: String):
 	var character_data = load_character_json(file_path)
 	var character = create_character(character_data)
@@ -27,19 +40,23 @@ static func create_character(character_data: Dictionary):
 	character_root.name = character_data["name"]
 	return character_root
 
+static func b64_to_texture(b64_png: String, size: Vector2) -> ImageTexture:
+	var buffer = Marshalls.base64_to_raw(b64_png)
+	var image = Image.new()
+	image.load_png_from_buffer(buffer)
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
+	texture.set_size_override(size)
+	return texture
+
 static func create_character_preview(character_data: Dictionary, size: Vector2=Vector2(200, 200)):
 	var animated_texture := AnimatedTexture.new()
-	animated_texture.frames = len(character_data["frames"])
+	animated_texture.frames = len(character_data["previewAnimation"])
 	
 	# Create textures from frames
 	var f = 0
-	for frame_data in character_data["frames"]:
-		var buffer = Marshalls.base64_to_raw(frame_data)
-		var image = Image.new()
-		image.load_png_from_buffer(buffer)
-		var texture = ImageTexture.new()
-		texture.create_from_image(image)
-		texture.set_size_override(size)
+	for frame_data in character_data["previewAnimation"]:
+		var texture := b64_to_texture(frame_data, Vector2(150,150))
 		animated_texture.set_frame_texture(f, texture)
 		f += 1
 
@@ -47,3 +64,21 @@ static func create_character_preview(character_data: Dictionary, size: Vector2=V
 	texture_rect.texture = animated_texture
 	
 	return texture_rect
+
+static func generate_character_sprite_frames(character_data: Dictionary) -> SpriteFrames:
+	var sprite_frames := SpriteFrames.new()
+	
+	for move in character_data["moveset"]:
+		var anim_name : String = move["type"]
+		sprite_frames.add_animation(anim_name) # light_punch, super, etc.
+		for frame in move["animation"]:
+			sprite_frames.add_frame(anim_name, b64_to_texture(frame["base64EncodedImage"], Vector2(300, 300)))
+	
+	for state in ["block", "crouch", "walk", "dash", "grappled", "hurt", "win", "lose"]:
+		sprite_frames.add_animation(state)
+		for frame in character_data[state + "Animation"]:
+			sprite_frames.add_frame(state, b64_to_texture(frame["base64EncodedImage"], Vector2(300, 300)))
+
+	
+	return sprite_frames
+	
