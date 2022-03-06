590ca373f524bd448041e3565685244614fe2157
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-05 23:22:18 -0500

    Generate animated texture from Base64

--- src/scenes/characters/manage_characters/ManageCharacter.gd
+++ src/scenes/characters/manage_characters/ManageCharacter.gd
@@ -2,21 +2,8 @@ extends Control
 
 export(PackedScene) var character_template: PackedScene
 
-func _create_character(character_data: Dictionary):
-	"""TODO"""
-	
-	var character_root := character_template.instance()
-	character_root.name = character_data["name"]
-	return character_root
 
-func _serialize_character(character_node):
-	var scene = PackedScene.new()
-	var result = scene.pack(character_node)
-	if result == OK:
-		var error := ResourceSaver.save("user://" + character_node.name + ".scn", scene)
-		if error != OK:
-			print(error)
-			push_error("An error occurred while saving the character to disk.")
+
 
 func _deserialize_characters():
 	var characters = []
@@ -33,7 +20,6 @@ func _deserialize_characters():
 	
 	return characters
 
-func _on_Import_pressed():
-	_serialize_character(_create_character({ 
-		"name": "No"
-	}))
+func _ready():
+	var animated_sprite = CharacterManager.create_character_preview(CharacterManager.load_character_json("out.json"))
+	$GridContainer.add_child(animated_sprite)
