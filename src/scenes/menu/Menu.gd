471138aa384c9bf570258289fbdf7f14ab552cea
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-10 23:15:20 -0500

    Menu button to testing scene

--- src/scenes/menu/Menu.gd
+++ src/scenes/menu/Menu.gd
@@ -8,9 +8,8 @@ func _stop_bg_music():
 func _ready():
 	first_focus.grab_focus()
 
-func _on_StartButton_pressed():
-#	get_tree().change_scene("res://levels/Level1.tscn")
-	pass
+func _on_TestingButton_pressed():
+	get_tree().change_scene("res://src/scenes/characters/test_characters/TestCharacters.tscn")
 
 func _on_QuitButton_pressed():
 	# Quit the game
