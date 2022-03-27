e05efb8414e4945d2bb38a120e765edc37d20d5c
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-26 22:19:55 -0400

    Add player overlays and selections

--- src/scenes/characters/select_characters/SelectCharacters.gd
+++ src/scenes/characters/select_characters/SelectCharacters.gd
@@ -1,6 +1,17 @@
 extends Control
 
 export(Vector2) var character_texture_size := Vector2(200, 200)
+export(Texture) var player1_overlay_texture
+export(Texture) var player2_overlay_texture
+
+onready var player1_overlay = $Player1Overlay
+onready var player2_overlay = $Player2Overlay
+
+var _character_files
+var _character_count := 0
+
+var _player1_index = null
+var _player2_index = null
 
 func _generate_animated_texture(character_data: Dictionary, size: Vector2) -> AnimatedTexture:
 	"""Given a character dictionary, generates an AnimatedTexture of the character's idle animation in the given size."""
@@ -19,9 +30,22 @@ func _generate_animated_texture(character_data: Dictionary, size: Vector2) -> An
 	return anim_texture
 
 func _ready():
+	CharacterManager.player1 = null
+	CharacterManager.player2 = null
+	
+	player1_overlay.texture = player1_overlay_texture
+	player1_overlay.expand = true
+	player1_overlay.rect_size = character_texture_size
+	
+	player2_overlay.texture = player2_overlay_texture
+	player2_overlay.expand = true
+	player2_overlay.rect_size = character_texture_size
+	
+
 	# Iterate through character files
-	var character_files = CharacterManager.list_character_files()
-	for character_file in character_files:
+	_character_files = CharacterManager.list_character_files()
+	_character_count = _character_files.size()
+	for character_file in _character_files:
 		var character_data = CharacterManager.load_character_json("res://test_characters/" + character_file)
 		var anim_texture := _generate_animated_texture(character_data, character_texture_size)
 		
@@ -31,3 +55,41 @@ func _ready():
 		texture_rect
 		
 		$GridContainer.add_child(texture_rect)
+
+func _handle_player_input(player_num: int, change: int):
+	if player_num == 1:
+		if _player1_index == null:
+			_player1_index = 0
+		else:
+			_player1_index = (_player1_index - 1) % _character_count
+		
+		player1_overlay.visible = true	
+		player1_overlay.rect_global_position = $GridContainer.get_children()[_player1_index].rect_global_position
+	elif player_num == 2:
+		if _player2_index == null:
+			_player2_index = 0
+		else:
+			_player2_index = (_player2_index - 1) % _character_count
+
+		player2_overlay.visible = true	
+		player2_overlay.rect_global_position = $GridContainer.get_children()[_player2_index].rect_global_position
+
+func _process(delta):
+	if Input.is_action_just_pressed("player_1_move_left"):
+		_handle_player_input(1, -1)
+	elif Input.is_action_just_pressed("player_1_move_right"):
+		_handle_player_input(1, 1)
+	
+	if Input.is_action_just_pressed("player_2_move_left"):
+		_handle_player_input(2, -1)
+	elif Input.is_action_just_pressed("player_2_move_right"):
+		_handle_player_input(2, 1)
+	
+	if Input.is_action_just_pressed("player_1_light_punch"):
+		CharacterManager.player1 = _character_files[_player1_index]
+	
+	if Input.is_action_just_pressed("player_2_light_punch"):
+		CharacterManager.player2 = _character_files[_player2_index]
+	
+	if CharacterManager.player1 and CharacterManager.player2:
+		get_tree().change_scene("res://src/scenes/characters/test_characters/TestCharacters.tscn")
