6682de838057f608dbcc23f7446a01f0ef203dea
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-12 11:31:26 -0400

    Refactor variable names

--- src/scripts/CharacterManager.gd
+++ src/scripts/CharacterManager.gd
@@ -10,10 +10,11 @@ var player2 = null
 
 const character_template = preload("res://src/actors/players/CharacterTemplate.tscn")
 
-# The recognized state animations in the exact ORDER they must be processed in
+# The recognized character state in the exact ORDER they must be processed in
 # This order ensures the spritesheet can be generated in sync with the animation tree
-const state_animation_names = [
+const STATES = [
 	"idle",
+	"enter",
 	"walk",
 	"dash",
 	"jump",
@@ -136,7 +137,7 @@ static func create_character(character_data: Dictionary):
 	var frame_index := 0
 	
 	# Create state animations
-	for state in state_animation_names:
+	for state in STATES:
 		var anim_name: String = state
 		
 		var animation_result := generate_animation(anim_name, character_data["stateAnimations"][state], frame_index)
@@ -215,7 +216,7 @@ static func generate_character_sprite_sheet(character_data: Dictionary) -> Dicti
 	var state_frame_count := 0
 	var action_frame_count := 0
 	
-	for state in state_animation_names:
+	for state in STATES:
 		frame_count += len(character_data["stateAnimations"][state])
 		state_frame_count += len(character_data["stateAnimations"][state])
 	
@@ -232,7 +233,7 @@ static func generate_character_sprite_sheet(character_data: Dictionary) -> Dicti
 
 	# Copy each frame into the spritesheet image
 	var frame_index = 0
-	for state in state_animation_names:
+	for state in STATES:
 		for frame in character_data["stateAnimations"][state]:
 			var frame_texture := frame_to_texture(frame, Vector2(frame_size, frame_size))
 			var frame_image := frame_texture.get_data()
